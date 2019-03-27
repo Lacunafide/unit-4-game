f169fb84cf79f33543c3d866d53aff02bfb3c394
Mike Martin <Lacunafide@gmail.com>
2019-03-26 22:20:51 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,8 @@
-var targetNumber = 0;
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+var targetScore = 0;
 var wins = 0;
 var losses = 0;
 var crystal1 = 0;
@@ -6,47 +10,64 @@ var crystal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
 var totalScore = 0;
-var newGame = true;
-console.log("javascript is connected");
+var newGame = false;
 
-// generates target number
-if (newGame) {
-    targetNumber = Math.floor(Math.random() * 50) + 10;
-    totalScore = 0;
+
+console.log("javascript is connected");
+targetScore = Math.floor(Math.random() * 50) + 10;
+crystal1 = Math.floor(Math.random() * 10 + 1);
+crystal2 = Math.floor(Math.random() * 10 + 1);
+crystal3 = Math.floor(Math.random() * 10 + 1);
+crystal4 = Math.floor(Math.random() * 10 + 1);
+document.getElementById("targetScore").innerHTML = "Target Score: " + targetScore;
+console.log(targetScore);
+console.log(crystal1);
+
+$(".button").on("click", function() {
+    if (totalScore == targetScore) {
+    wins ++;
+    newGame = true;
+    targetScore = Math.floor(Math.random() * 50) + 10;
     crystal1 = Math.floor(Math.random() * 10 + 1);
     crystal2 = Math.floor(Math.random() * 10 + 1);
     crystal3 = Math.floor(Math.random() * 10 + 1);
     crystal4 = Math.floor(Math.random() * 10 + 1);
-    newGame = false;
-    console.log(targetNumber);
-}
+    document.getElementById("targetScore").innerHTML = "Target Score: " + targetScore;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    totalScore = 0;
+    }
+})
 
-if (totalScore === targetScore) {
-    wins ++;
-    newGame = true;
-}
-
-if (totalScore > targetScore) {
+$(".button").on("click", function() {
+    if (totalScore > targetScore) {
+    targetScore = Math.floor(Math.random() * 50) + 10;
+    crystal1 = Math.floor(Math.random() * 10 + 1);
+    crystal2 = Math.floor(Math.random() * 10 + 1);
+    crystal3 = Math.floor(Math.random() * 10 + 1);
+    crystal4 = Math.floor(Math.random() * 10 + 1);
+    document.getElementById("targetScore").innerHTML = "Target Score: " + targetScore;
     losses ++;
+    totalScore = 0;
     newGame = true;
-}
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    }
+})
+
 
 $("#crystal1").on("click", function() {
-    totalScore + crystal1;
-    console.log(totalScore);
+    totalScore += crystal1;
+    document.getElementById("totalScore").innerHTML = "Total Score: " + totalScore;
 })
 $("#crystal2").on("click", function() {
-    totalScore + crystal2;
+    totalScore += crystal2;
+    document.getElementById("totalScore").innerHTML = "Total Score: " + totalScore;
 })
 $("#crystal3").on("click", function() {
-    totalScore + crystal3;
+    totalScore += crystal3;
+    document.getElementById("totalScore").innerHTML = "Total Score: " + totalScore;
 })
 $("#crystal4").on("click", function() {
-    totalScore + crystal4;
+    totalScore += crystal4;
+    document.getElementById("totalScore").innerHTML = "Total Score: " + totalScore;
 })
-
-document.getElementById("#wins").innerHTML = wins;
-document.getElementById("#losses").innerHTML = losses;
-document.getElementById("#targetScore").innerHTML = targetScore;
-document.getElementById("#totalScore").innerHTML = totalScore;
 
